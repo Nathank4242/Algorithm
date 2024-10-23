9cc80c3dde055959c05920bc39e40a628deb0680
Nathank4242 <nkweon@gmail.com>
2024-10-23 13:58:35 -0400

    algorithm

--- parking.js
+++ parking.js
@@ -1,6 +1,11 @@
 function parking(number, yesterday, today) {
-    let result = 0;
-  for (let i = 1; i <= number; i++) {
-    if (yesterday [i] && )
+  let result = 0;
+  for (let i = 0; i < number; i++) {
+    if (yesterday[i] === "C" && today[i] === "C") {
+      result++;
+    }
   }
+  return result;
 }
+
+console.log(parking(5, ".CC..", " CC..C"));
